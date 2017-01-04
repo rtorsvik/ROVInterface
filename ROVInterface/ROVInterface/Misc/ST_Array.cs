b8e21cbb51bb3f6d26b2b1b7ac5ff8d0b4e146bc
Goolygot <terje_1995@hotmail.com>
2017-01-04 15:27:31 +0100

    Restructure

---
+++ ROVInterface/ROVInterface/Misc/ST_Array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ST_Array {
+	private arrelement[] arr;
+	// Indexer, so this array instance can be used as a[i];
+	public arrelement this[int i] {
+		get {
+			return null;
+		}
+		set {
+
+		}
+	}
+
+	public class arrelement {
+		private int _index;
+
+		public int index { get { return _index; } set { _index = value; } }
+	}
+}
