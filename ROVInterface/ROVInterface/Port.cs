31a5deddf1b01b8cc8ba4d0b0313fc8d2d77ffff
rtorsvik <rein.torsvik@hotmail.com>
2017-01-06 13:51:50 +0100

    Created new interface for port connections

---
+++ ROVInterface/ROVInterface/Port.cs
@@ -0,0 +1,17 @@
+
+
+
+public Interface Port
+{
+	
+	public bool Send();
+	public bool Recive();
+	
+	public bool Open();
+	public bool IsOpen();
+	public bool Close();
+	
+	public string portName();
+  
+
+}
