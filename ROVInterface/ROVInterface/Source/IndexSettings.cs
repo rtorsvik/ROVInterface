3ee1f990cea2055f00a41403a161a9dbcf8e7998
Goolygot <terje_1995@hotmail.com>
2017-01-09 13:06:09 +0100

    -

--- ROVInterface/ROVInterface/Source/IndexSettings.cs
+++ ROVInterface/ROVInterface/Source/IndexSettings.cs
@@ -4,6 +4,8 @@ using System.Windows.Forms;
 class IndexSettings {
 
 	private FlowLayoutPanel CreateElement() {
+		Setting setting = new Setting();
+
 		FlowLayoutPanel temp = new FlowLayoutPanel();
 		// Create index and parent it to temp
 		// Create name and parent it to temp
@@ -13,12 +15,29 @@ class IndexSettings {
 		// Create delete button and parent it to temp, add event for click to Delete(parent)
 		NumericUpDown nud = new NumericUpDown();
 		nud.Parent = temp;
+		nud.ValueChanged += setting.UpdateIndex;
 
+		TextBox txtbox = new TextBox();
+		txtbox.Parent = temp;
+		txtbox.TextChanged += setting.UpdateStats;
 
 		return temp;
 	}
 
 	private void Delete(FlowLayoutPanel panel) {
 		// Delete the panel
+		
+	}
+
+	public class Setting {
+
+		
+		public void UpdateIndex(object sender, EventArgs e) {
+
+		}
+
+		public void UpdateStats(object sender, EventArgs e) {
+
+		}
 	}
 }
