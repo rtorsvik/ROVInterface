f64db5072a1b89504c7a449c4c6f8c4d11d97812
Goolygot <terje_1995@hotmail.com>
2017-01-23 12:00:11 +0100

    added aegir.dll

---
+++ ROVInterface/AEgir/main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AEgir {
+    public static class main {
+
+        private static int i = 0;
+
+        public static int returnint() {
+            return ++i;
+        }
+    }
+}
