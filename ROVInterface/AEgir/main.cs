c2720ef67828e31becdb027b6abafd12792f5b43
Goolygot <terje_1995@hotmail.com>
2017-01-23 12:47:52 +0100

    .

--- ROVInterface/AEgir/main.cs
+++ ROVInterface/AEgir/main.cs
@@ -12,5 +12,31 @@ namespace AEgir {
         public static int returnint() {
             return ++i;
         }
+
+        /*
+            Need two functions
+            byte[] ConvertCommands(int index, int value);
+            Pair<int,int> ConvertData(byte[] b);
+        */
+
+        public static byte[] ConvertCommands(ushort index, int value) {
+
+            byte[] packet = new byte[6];
+
+            packet[0] = (byte)(index >> 8);
+            packet[1] = (byte)(index >> 0);
+            
+            packet[5] = (byte)(value >> 24);
+            packet[4] = (byte)(value >> 16);
+            packet[3] = (byte)(value >> 8);
+            packet[2] = (byte)(value >> 0);
+
+            return packet;
+        }
+
+        public static KeyValuePair<int, int> ConvertData(byte[] b) {
+
+            return new KeyValuePair<int,int>(0, -1);
+        }
     }
 }
