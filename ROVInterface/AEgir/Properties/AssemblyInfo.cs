9422590b3b15ff053755e64b65002cc733604399
Goolygot <terje_1995@hotmail.com>
2017-01-26 11:48:24 +0100

    .

--- ROVInterface/AEgir/Properties/AssemblyInfo.cs
+++ ROVInterface/AEgir/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AEgir")]
+[assembly: AssemblyTitle("Translator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AEgir")]
+[assembly: AssemblyProduct("Translator")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
