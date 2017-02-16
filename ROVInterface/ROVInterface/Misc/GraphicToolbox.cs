a5d959b58b9bc29e69b046e762f05b1f9c551d89
Goolygot <terje_1995@hotmail.com>
2017-02-16 11:12:56 +0100

    Added GraphicToolbox.cs script.

---
+++ ROVInterface/ROVInterface/Misc/GraphicToolbox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public class GraphicToolbox {
+
+	private Control parentGraphic;
+	private Control parentToolbox;
+
+	public GraphicToolbox(Control parentGraphic, Control parentToolbox) {
+		this.parentGraphic = parentGraphic;
+		this.parentToolbox = parentToolbox;
+
+		// Create the toolbox items
+		CreateToolbox();
+	}
+
+	private void CreateToolbox() {
+
+	}
+}
